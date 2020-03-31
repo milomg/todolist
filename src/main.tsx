46cf0e360d35d494a36259c9ac5c6fa8908ce183
modderme123 <modderme123@gmail.com>
2020-03-31 16:44:55 -0700

    make times slip less

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import "./style.css";
-import { onCleanup, createState, createEffect } from "solid-js";
-import { render, For } from "solid-js/dom";
+import { onCleanup, createState, createEffect, createSignal } from "solid-js";
+import { render, For, Show } from "solid-js/dom";
 import { SetStateFunction, Wrapped } from "solid-js/types/state";
 
 function createLocalState<T>(initState: T): [Wrapped<T>, SetStateFunction<T>] {
   const [state, setState] = createState(initState);
-  if (localStorage.todos) setState(JSON.parse(localStorage.todos));
+  if (localStorage.todos) {
+    let parsed = JSON.parse(localStorage.todos);
+    parsed.time = new Date(parsed.time);
+    setState(parsed);
+  }
   createEffect(() => (localStorage.todos = JSON.stringify(state)));
   return [state, setState];
 }
@@ -28,61 +32,97 @@ function beep() {
   v.stop(a.currentTime + duration + 0.05);
 }
 
+function addSeconds(s: number): Date {
+  return new Date(Date.now() + s * 1000 - 1);
+}
+function deltaDisplay(d: Date): string {
+  let now = Math.max(d.getTime() - Date.now() + 1000, 0);
+  let seconds = Math.floor(now / 1000) % 60;
+  let min = Math.floor(now / 60000) % 60;
+  return min + ":" + (seconds + "").padStart(2, "0");
+}
+function displaySecs(s: number): string {
+  return Math.floor(s / 60) + ":" + (Math.floor(s % 60) + "").padStart(2, "0");
+}
+function posmod(n: number, m: number): number {
+  return ((n % m) + m) % m;
+}
 type Todo = { title: string; done: boolean; id: number };
 const App = () => {
   const [state, setState] = createLocalState({
-    time: 25 * 60,
+    time: addSeconds(25 * 60),
     newTitle: "",
     todos: [] as Todo[],
     nextId: 0,
-    paused: false,
+    paused: undefined as undefined | number,
     displayedNotification: false,
   });
-  const timer = setInterval(() => {
-    if (!state.paused) {
-      if (state.time > 0) setState("time", (t) => t - 1);
-      else if (!state.displayedNotification) {
+  const [toggle, setToggle] = createSignal(false);
+  const tick = () => {
+    setToggle(!toggle());
+    if (state.paused == undefined && Date.now() >= state.time.getTime()) {
+      if (!state.displayedNotification) {
         setState("displayedNotification", true);
         new Notification("The timer is up!");
-      } else {
-        a.resume();
-        beep();
-        setTimeout(() => beep(), 200);
-        setTimeout(() => beep(), 400);
-        setTimeout(() => beep(), 600);
       }
+      a.resume();
+      beep();
+      setTimeout(() => beep(), 200);
+      setTimeout(() => beep(), 400);
+      setTimeout(() => beep(), 600);
     }
-  }, 1000);
-  onCleanup(() => clearInterval(timer));
+  };
+
+  let timer: NodeJS.Timeout | undefined = undefined;
+  setTimeout(() => {
+    if (!timer) timer = setInterval(tick, 1000);
+  }, posmod(state.time.getTime() - Date.now(), 1000));
+  onCleanup(() => clearInterval(timer!));
 
   return (
     <>
       <section class="hero">
         <div class="hero-body">
           <div class="container has-text-centered">
-            <h1 class="title is-1">{Math.floor(state.time / 60) + ":" + ((state.time % 60) + "").padStart(2, "0")}</h1>
+            <h1 class="title is-1">
+              {state.paused != undefined ? displaySecs(state.paused) : [toggle(), deltaDisplay(state.time)][1]}
+            </h1>
             <div class="field has-addons has-addons-centered">
               <div class="control">
                 <button
                   class="button"
                   onClick={() => {
-                    setState("paused", (p) => !p);
+                    if (state.paused != undefined) {
+                      setState("time", addSeconds(state.paused));
+                      setState("paused", undefined);
+                      clearInterval(timer!);
+                      timer = setInterval(tick, 1000);
+                    } else {
+                      setState("paused", Math.max((state.time.getTime() - Date.now()) / 1000, 0));
+                    }
                   }}
                 >
-                  {state.paused ? "Play" : "Pause"}
+                  {state.paused == undefined ? "Pause" : "Play"}
                 </button>
               </div>
               <div class="control">
                 <button
                   class="button"
                   onClick={() => {
-                    setState("time", 10);
+                    setState("paused", 10);
                     setState("displayedNotification", false);
                   }}
                 >
                   Reset
                 </button>
               </div>
+              <Show when={!state.paused && state.time.getTime() <= Date.now()}>
+                <div class="control">
+                  <button class="button" onClick={() => setState("todos", [])}>
+                    Clear Todos
+                  </button>
+                </div>
+              </Show>
             </div>
           </div>
         </div>
@@ -104,7 +144,11 @@ const App = () => {
                 class="button is-info"
                 onClick={() => {
                   setState((s) => {
-                    s.todos.push({ title: state.newTitle, done: false, id: s.nextId });
+                    s.todos.push({
+                      title: state.newTitle,
+                      done: false,
+                      id: s.nextId,
+                    });
                     s.newTitle = "";
                     s.nextId++;
                   });
@@ -118,14 +162,15 @@ const App = () => {
             {(todo) => (
               <div class="field has-addons has-addons-centered">
                 <div class="control">
-                  <input
-                    type="checkbox"
-                    checked={todo.done}
+                  <div
+                    class="button"
                     onClick={(e) => {
                       const idx = state.todos.findIndex((t) => t.id === todo.id);
-                      setState("todos", idx, { done: e.target.checked });
+                      setState("todos", idx, "done", (done: boolean) => !done);
                     }}
-                  ></input>
+                  >
+                    <input type="checkbox" checked={todo.done}></input>
+                  </div>
                 </div>
                 <div class="control">
                   <input
